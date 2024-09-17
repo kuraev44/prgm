064abd6da5ef5eb1a8d9d685fce8bcb84a74abae
kuraev44 <star151204@gmail.com>
2024-09-17 15:08:14 +0700

    Добавили оператор switch and if

--- pr003.java
+++ pr003.java
@@ -6,7 +6,70 @@ class pr003 {
 	       System.out.print("Нажмите клавишу на клавиатуре и ENTER ");
 	       ch = (char) System.in.read();
 	       System.out.println("Вы нажали клавишу: " + ch);
-       	       
+
+	       //угадывание буквы
+	       //пример использования проверки условия
+	       char answer = 'r';
+	       if (ch == answer)
+		       System.out.println("Буква указана верно");
+	       else
+		       System.out.println("Буква указана неверно");
+	       System.out.println("/n");
+	       //работа с вложенными операторами if
+	       int i=10, j=10, k=110, a=5, b=15, c=25, d=35;
+	       if(i==10) {
+		       if(j < 20)
+			       a = b;
+		       if(k > 100)
+			       c = d;
+		       else 
+			       a = c;
+	       }	       
+	       else a = d;
+	       System.out.println(" a = " + a + " c= " + c + " d= " + d);
+
+	       System.out.println("/n");
+	       //цепочка if else if
+	       int x;
+	       for (x = 0; x < 6; x++) {
+		       if (x == 1)
+			       System.out.println("Значение равно 1");
+		       else if (x == 2)
+			       System.out.println("Значение равно 2");
+		       else if (x == 3)
+                               System.out.println("Значение равно 3");
+		       else if (x == 4)
+                               System.out.println("Значение равно 4");
+		       else
+			       System.out.println("Значение x не находится между 1 и 4");
+		}
+
+		//оператор switch
+		System.out.println("\n");
+		int ii;
+		for (ii=0; ii<10; ii++)
+			switch(ii) {
+				case 0:
+					System.out.println("ii равно 0");
+					break;
+				case 1:
+					System.out.println("ii равно 1");
+                                        break;
+
+				case 2:
+					System.out.println("ii равно 2");
+                                        break;
+
+				case 3:
+					System.out.println("ii равно 3");
+                                        break;
+
+				case 4:
+					System.out.println("ii равно 4");
+                                        break;
+				default:
+					System.out.println("ii больше или равно 5");
+			}
 	
 	
 	}
