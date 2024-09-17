ec97c12be3439db2ba0414158d3bcc15e69372a6
kuraev44 <star151204@gmail.com>
2024-09-17 13:34:53 +0700

    Добавили чтение клавиатуры

---
+++ pr003.java
@@ -0,0 +1,13 @@
+class pr003 {
+	public static void main(String args[]) throws java.io.IOException {
+
+		//чтение с клавы
+	       char ch;
+	       System.out.print("Нажмите клавишу на клавиатуре и ENTER ");
+	       ch = (char) System.in.read();
+	       System.out.println("Вы нажали клавишу: " + ch);
+       	       
+	
+	
+	}
+}
