aed21f58b82fd2a005ec2f3a936bd0233a831ff7
kuraev44 <star151204@gmail.com>
2024-09-24 13:05:50 +0700

    Варианты использования цикла for

--- pr003.java
+++ pr003.java
@@ -89,5 +89,37 @@ class pr003 {
 				System.out.println("Не выведется");
 				break;
 		}
+		//Операто цикла for
+		//
+		System.out.println("\n");
+		double num, sroot, rerr;
+
+		for(num=1.0; num < 100.0; num++) {
+			sroot = Math.sqrt(num);
+			System.out.println("квадратный корень числа " + num + " Равен " + sroot);
+			//Вычисление ошибки округления
+			rerr = num - (sroot * sroot);
+			System.out.println("Ошибка округления составляет " + rerr);
+			System.out.println();
+		}
+
+		int x1;
+
+		for (x1 = 100; x1 > -100; x1 -= 5)
+			System.out.println(x1);
+
+		System.out.println("\n");
+		
+		int i7, j7;
+		for (i7 = 0, j7 = 10; i7 < j7; i7++, j7--)
+			System.out.println("i7 и j7: " + i7 + " " + j7);
+
+		System.out.println("\n");
+
+
+		int i8;
+		System.out.println("Для остановки цикла нажмите клавишу \"S\"");
+		for (i8 = 0; (char) System.in.read() != 'S'; i8++)
+			System.out.println("итерация №" + i8);
 	}
 }
