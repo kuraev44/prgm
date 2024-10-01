3dc10a63378d157ebcc8fc9cceefc4e4377c2ad5
kuraev44 <star151204@gmail.com>
2024-10-01 15:04:55 +0700

    Объявили класс и создали его экземпляр

--- pr004.java
+++ pr004.java
@@ -7,17 +7,28 @@ class pr004 {
 	public static void main(String args[]) {
 		//создание экз класса
 		Avtomobil lada = new Avtomobil();
-		int rasst;
+		Avtomobil porshe = new Avtomobil();
+		int rasst, rasst2;
 
 		//присваиваем значение
 		lada.pass = 4;
 		lada.v = 40;
 		lada.rash = 9.5;
 
+		porshe.pass = 1;
+		porshe.v = 100;
+		porshe.rash = 14.0;
+
+
+
 		//рассчет расстояния коорое лада проедет на полном баке
 
 		rasst = (int) (lada.v/lada.rash) * 100;
+		rasst = (int) (porshe.v/porshe.rash) * 100;
+
 
 		System.out.println("Lada проедет " + rasst + " км. на полном баке");
+		System.out.println("Porshe проедет " + rasst + " км. на полном баке");
+
 	}
 }	
