f4130c0a49e5de060b73a220d8d2e7f2401a5c4e
kuraev44 <star151204@gmail.com>
2024-10-08 12:57:54 +0700

    Возврат значений через методы

--- pr004.java
+++ pr004.java
@@ -7,6 +7,15 @@ class Avtomobil {
 	void rasst() {
 		System.out.println("Расстояние на полном баке: " + ((int)(v/rash * 100)));
 	}
+
+	int rasst2() {
+		return (int)(v/rash * 100);
+
+	}
+	double fuelneeded(int km) {
+		return ((double)km/100)*rash;
+	
+	}
 }
 class pr004 {
 	public static void main(String args[]) {
@@ -18,11 +27,16 @@ class pr004 {
 		//присваиваем значение
 		lada.pass = 4;
 		lada.v = 40;
-		lada.rash = 9.5;
+		lada.rash = 5.5;
 
 		porshe.pass = 1;
 		porshe.v = 100;
 		porshe.rash = 14.0;
+
+		System.out.println("Для расстояни в 10 км нужно " + lada.fuelneeded(10) + "литров бензина");
+		rasst = 10;
+		System.out.println("Для расстояни в 10 км нужно " + lada.fuelneeded(rasst) + "литров бензина");
+
 
 		//рассчет расстояния коорое лада проедет на полном баке
 
@@ -32,9 +46,14 @@ class pr004 {
 
 		System.out.print("для Lada: " );
 		lada.rasst();
+		rasst = lada.rasst2();
+		System.out.println("Результат rasst2():" + lada.rasst2() );
+
 
 		System.out.println("Для Porshe: ");
 		porshe.rasst();
-
+		System.out.println("Результат rasst2():" + porshe.rasst2() );
+		
+		
 	}
 }	
