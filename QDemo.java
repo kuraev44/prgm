d84ced4309b63fdc1d727f4f8a4bdf590a8712b9
kuraev44 <star151204@gmail.com>
2024-11-05 15:06:34 +0700

    Добавили конструкторы на базе массива и другой очереди

--- QDemo.java
+++ QDemo.java
@@ -1,11 +1,33 @@
 class queue {
 	private char[] q;
 	private int putloc, getloc; //указатели на элементы очереди
-	
-	queue (int size) {
+
+	//
+	public queue (int size) {
 		q = new char[size];
 		putloc = getloc = 0;
 	
+	}
+	
+	//
+	queue(queue ob) {
+		// утснаовка
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		//
+		q = new char[ob.q.length];
+		//
+		for(int i=getloc; i < putloc;  i++)
+			q[i] = ob.q[i];
+	}
+	//
+	queue(char[] a) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+
+		for(int i = 0; i < a.length; i++)
+			put(a[i]);
 	}
 
 	//метод для помещения символа в очередь
@@ -66,9 +88,40 @@ class QDemo {
 		for(i=0; i < 5; i++) {
 			ch = smallQ.get();
 			if(ch != (char) 0)System.out.print(ch);
-
 		}
+		
+		queue q1 = new queue(10);
+		char[] name = {'I', 'v', 'a', 'n'};
 
+		queue q2 = new queue(name);
+		
+		//char ch;
+		int i2;
 
+		for(i2=0; i2<10; i2++)
+			q1.put((char) ('A' + i2));
+		
+		queue q3 = new queue(q1);
+
+		System.out.print("q1: ");
+		for(i2 = 0; i2<10; i2++) {
+			ch = q1.get();
+			System.out.print(ch);
+		}
+		System.out.println("\n");
+		
+		System.out.print("q2: ");
+                for(i2 = 0; i2<10; i2++) {
+                        ch = q2.get();
+                        System.out.print(ch);
+		}
+		System.out.println("\n");
+
+		System.out.print("q3: ");
+                for(i2 = 0; i2<10; i2++) {
+                        ch = q3.get();
+                        System.out.print(ch);
+		}
+		System.out.println("\n");
 	}
 }
