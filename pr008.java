3cd1feae24f4b95ea66bd6efb90a4a7f87ebca97
kuraev44 <star151204@gmail.com>
2024-11-12 13:09:21 +0700

    Добавили демонстрацию работы переменной длины

--- pr008.java
+++ pr008.java
@@ -38,7 +38,88 @@ class StaticBlock {
 		System.out.println(msg);
 	}
 }
+class Outer {
+	int[] nums;
+	Outer(int[] n) {
+		nums = n;
+	}
 
+	void analyze() {
+		Inner inOb = new Inner();
+		System.out.println("Минимальное значение: " + inOb.min());
+		System.out.println("Максимальное значение: " + inOb.max());
+		System.out.println("Среднее значение: " + inOb.avg());
+	}
+
+	//внутренний класс
+	class Inner {
+		int min() {
+			int m = nums[0];
+			for(int i=1; i<nums.length; i++)
+				if(nums[i] < m) m = nums[i];
+			return m;
+		}
+		int max() {
+                        int m = nums[0];
+                        for(int i=1; i<nums.length; i++)
+                                if(nums[i] > m) m = nums[i];
+                        return m;
+                }
+		int avg() {
+			int a = 0;
+                        int m = nums[0];
+                        for(int i=1; i<nums.length; i++)
+                                a += nums[i];
+                        return a/nums.length;
+                }
+
+
+	}
+}
+class VarArgs {
+	static void vaTest(int ... v) {
+		System.out.println("Кол-во аргументов: " + v.length);
+		System.out.println("Содержимое массива аргументов: ");
+		for(int i=0; i<v.length; i++)
+			System.out.println(" аргумент " + i + ": " + v[i]);
+		System.out.println();
+
+	}
+	static void vaTest2(String msg, int ... v) {
+		System.out.println(msg + v.length);
+		System.out.println("Содержимое массива аргументов:\n ");
+                for(int i=0; i<v.length; i++)
+                        System.out.println(" аргумент " + i + ": " + v[i]);
+                System.out.println();
+	}
+	static void vaTest3(int ... v) {
+                System.out.println("vaTest3(int ...)" + "Кол-во аргументов" + v.length);
+                System.out.println("Содержимое массива аргументов:\n ");
+                for(int i=0; i<v.length; i++)
+                        System.out.println(" аргумент " + i + ": " + v[i]);
+                System.out.println();
+	}
+	static void vaTest3( boolean ... v) {
+                System.out.println("vaTest3(boolean ...)" + "Кол-во аргументов: " + v.length);
+                System.out.println("Содержимое массива аргументов:\n ");
+                for(int i=0; i<v.length; i++)
+                        System.out.println(" аргумент " + i + ": " + v[i]);
+                System.out.println();
+	}
+	static void vaTest3(String msg, int ... v) {
+                System.out.println("vaTest3(String, int ...)" + msg + v.length);
+                System.out.println("Содержимое массива аргументов: ");
+                for(int i=0; i<v.length; i++)
+                        System.out.println(" аргумент " + i + ": " + v[i]);
+                System.out.println();
+
+
+
+	
+
+
+	}
+}
 
 class pr008 {
 	public static void main(String[] args) {
@@ -85,9 +166,58 @@ class pr008 {
 		System.out.println("Квадратный корень из 3: " + StaticBlock.rootOf3);
 
 
+		//демонстрация работы внутреннего класса
+		int[] x = {3, 2, 1, 5, 6, 9, 7, 2};
+		Outer outOb = new Outer(x);
 
+		outOb.analyze();
 
+		class ShowB {
+		        int numbits;
+      			ShowB(int n) {
+               			 numbits = n;
+        		}
+        		void show(long val) {
+                		long mask = 1;
+                //сдвиг влево на значении 1
+               			mask <<= numbits-1;
 
+                		int spacer = 0;
+               			for(;mask!=0;mask>>>=1) {
+                        		if((val & mask) !=0)
+                                		System.out.print("1");
+                        		else System.out.print("0");
+                        		spacer++;
+                        		if((spacer%8) == 0) {
+                                		System.out.print(" ");
+                                		spacer = 0;
+                        		}
+                		}
+        		System.out.println();
+        		}
+		}
+		for(byte b=0; b<10; b++) {
+			ShowB byteval = new ShowB(8);
+			System.out.print(b + " в двоичном виде: ");
+			byteval.show(b);
+
+		}
+
+		System.out.println();
+		//
+		VarArgs.vaTest(10);
+		VarArgs.vaTest(1, 2, 3);
+		VarArgs.vaTest();
+
+		System.out.println();
+		VarArgs.vaTest2("Один аргумент: ", 10);
+                VarArgs.vaTest2("Два аргумента: ", 1, 2, 3);
+                VarArgs.vaTest2("Без аргументов переменной длины ");
+
+		System.out.println();
+                VarArgs.vaTest3(10, 20);
+                VarArgs.vaTest3("Два аргумента: ", 1, 2, 3);
+                VarArgs.vaTest3("true, false, false");
 
 	}
 }
