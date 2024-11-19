c396cf02b856e7c01c080fb579e8025e3069cd6c
kuraev44 <star151204@gmail.com>
2024-11-19 10:42:11 +0700

    Добавили подкласс Rectangle

--- pr009.java
+++ pr009.java
@@ -17,16 +17,39 @@ class Triangle extends TwoDShape {
 		System.out.println("Стиль: " + style);
 	}
 }
+class Rectangle extends TwoDShape {
+	String outline;
+
+	double area() {
+		return width*height;
+	}
+	boolean isSquare() {
+		if(width == height) return true;
+		return false;
+	}
+	void showOutline() {
+                System.out.println("Рамка: " + outline);
+        }
+
+}
 class pr009 {
 	public static void main(String[] args) {
 		Triangle t1 = new Triangle();
 		Triangle t2 = new Triangle();
+		Rectangle r1 = new Rectangle();
+		Rectangle r2 = new Rectangle();
 		t1.width = 4.0;
 		t1.height = 4.0;
 		t1.style = "закрашенный";
 		t2.width = 8.0;
 		t2.height = 12.0;
 		t2.style = "конутрный";
+		r1.width = 4.0;
+		r1.height = 4.0;
+		r1.outline = "сплошная";
+		r2.width = 8.0;
+                r2.height = 12.0;
+                r2.outline = "Пунктирная";
 		System.out.println("Информация об объекте t1: ");
 		t1.showStyle();
 		t1.showDim();
@@ -37,6 +60,25 @@ class pr009 {
                 t2.showDim();
                 System.out.println("Площадь: " + t2.area());
                 System.out.println();
+		System.out.println("Информация об объекте r1: ");
+		r1.showOutline();
+                r1.showDim();
+		if(r1.isSquare())
+			System.out.println("Является квадратом");
+		else
+			System.out.println("Не является квадратом");
+		System.out.println("Площадь: " + r1.area());
+		System.out.println();
+                System.out.println("Информация об объекте r2: ");
+		r2.showOutline();
+                r2.showDim();
+		if(r2.isSquare())
+                        System.out.println("Является квадратом");
+                else
+                        System.out.println("Не является квадратом");
+		System.out.println("Площадь: " + r2.area());
+                System.out.println();
+
 	}
 }
 
