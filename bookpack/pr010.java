81b0f4943ca61877ca5f5277d6d10288e9b24007
kuraev44 <star151204@gmail.com>
2024-12-03 12:34:58 +0700

    Добавили пример работы с пакетами

---
+++ bookpack/pr010.java
@@ -0,0 +1,33 @@
+package bookpack;
+class Book {
+	private String title;
+	private String author;
+	private int pubDate;
+
+	Book(String t, String a, int d) {
+		title = t;
+		author = a;
+		pubDate = d;
+	}
+
+	void show() {
+		System.out.println(title);
+		System.out.println(author);
+		System.out.println(pubDate);
+		System.out.println();
+	}
+}
+
+class pr010 {
+	public static void main(String[] args) {
+		Book[] books = new Book[5];
+		books[0] = new Book("Иллиада", "Гомер", 1980);
+		books[1] = new Book("Унесенные ветром", "Митчел", 2000);
+		books[2] = new Book("Наедине с собой", "Аврелий", 1950);
+		books[3] = new Book("Собор Парижской юогоматери", "Гюго", 1970);
+		books[4] = new Book("Преступление и наказание", "Достоевский", 1950);
+		for(int i=0; i < books.length; i++)
+			books[i].show();	
+	}
+
+}
